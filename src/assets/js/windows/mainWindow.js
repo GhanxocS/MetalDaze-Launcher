5ba605d50657c9cc32bf5d6a13e78c998cb3ef4b
ITakerMetal <rodrigororodiaz@gmail.com>
2026-04-27 20:12:35 -0400

    Initial commit - MetalDaze Launcher

---
+++ src/assets/js/windows/mainWindow.js
@@ -0,0 +1,54 @@
+/**
+ * @author Luuxis
+ * Luuxis License v1.0 (voir fichier LICENSE pour les détails en FR/EN)
+ */
+
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const os = require("os");
+let dev = process.env.DEV_TOOL === 'open';
+let mainWindow = undefined;
+
+function getWindow() {
+    return mainWindow;
+}
+
+function destroyWindow() {
+    if (!mainWindow) return;
+    app.quit();
+    mainWindow = undefined;
+}
+
+function createWindow() {
+    destroyWindow();
+    mainWindow = new BrowserWindow({
+        title: app.getName(),
+        width: 480,
+        height: 650,
+        minWidth: 480,
+        minHeight: 650,
+        resizable: false,
+        icon: `./src/assets/images/icon/icon.${os.platform() === "win32" ? "ico" : "png"}`,
+        frame: false,
+        show: false,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        },
+    });
+    Menu.setApplicationMenu(null);
+    mainWindow.setMenuBarVisibility(false);
+    mainWindow.loadFile(path.join(`${app.getAppPath()}/src/launcher.html`));
+    mainWindow.once('ready-to-show', () => {
+        if (mainWindow) {
+            if (dev) mainWindow.webContents.openDevTools({ mode: 'detach' })
+            mainWindow.show()
+        }
+    });
+}
+
+module.exports = {
+    getWindow,
+    createWindow,
+    destroyWindow,
+};
